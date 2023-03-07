606c070ed32d65e6d4182aecea0f455b011ec99b
Ryan Rodrigo <ryanr4057@gmail.com>
2023-03-07 20:01:11 -0300

    filtro de odds

--- bet365.py
+++ bet365.py
@@ -10,32 +10,37 @@ import time
 race_count = 1
 env_mens = []
 
+
 while True:
-   subprocess.Popen(
-      '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --remote-debugging-port=9222', shell=True)
-
-   options = webdriver.ChromeOptions()
-   options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-
-   driver = webdriver.Chrome(options=options)
-   driver.maximize_window()
-   driver.get('https://www.bet365.com/#/IP/B4')
-
-   time.sleep(5)
-
-   corrida = driver.find_elements(By.CLASS_NAME,"ovm-RacingViewAllLink ")
-
-   # print(len(corrida))
-
-   corrida[0].click()
-
-   time.sleep(3)
-
-   current_url = driver.current_url
-   driver.get(current_url)
-
-   time.sleep(5)
    try:
+      subprocess.Popen(
+         '"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" --remote-debugging-port=9222', shell=True)
+
+      options = webdriver.ChromeOptions()
+      options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+      driver = webdriver.Chrome(options=options)
+      driver.maximize_window()
+      driver.get('https://www.bet365.com/#/IP/B4')
+
+      time.sleep(5)
+
+      corrida = driver.find_elements(By.CLASS_NAME,"ovm-RacingViewAllLink ")
+
+      # print(len(corrida))
+      while len(corrida) ==0:
+         corrida = driver.find_elements(By.CLASS_NAME,"ovm-RacingViewAllLink ")
+         time.sleep(1)
+         
+      corrida[0].click()
+
+      time.sleep(3)
+
+      current_url = driver.current_url
+      driver.get(current_url)
+
+      time.sleep(5)
+      
       f.proc_avb_w(driver, race_count, env_mens)
 
    except Exception:
--- bet_funções.py
+++ bet_funções.py
@@ -22,14 +22,14 @@ def proc_prox_race(driver, race_count):
     if qtd_race < race_count:
         race_count = 0
     p_race[race_count].click()
-    time.sleep(3)
+    time.sleep(1)
 
     current_url = driver.current_url
     driver.get(current_url)
     if race_count < len(p_race) - 1:
         race_count = race_count + 1
     elif race_count == len(p_race) - 1:
-        time.sleep(30)
+        time.sleep(2)
         race_count = 0
     return driver, race_count
 
@@ -42,8 +42,6 @@ def proc_avb_w(driver, race_count, env_mens):
         all_dogs = driver.find_elements(By.CLASS_NAME,"ir-RacingGreyhoundsFixedOddsParticipant_ParticipantName ")
         for i in range(0, len(all_dogs) -1):
             n_dogs.append(all_dogs[i].text)
-
-        # print(n_dogs)
 
         if len(avb_dogs) > 0:
             html = driver.page_source
@@ -59,7 +57,6 @@ def proc_avb_w(driver, race_count, env_mens):
                 r_dist = fbd.buscar_race_dist(race_id)
                 r_cat = fbd.buscar_race_cat(race_id)
                 for i in range(0, len(dogs), 2):
-                    # print(f"{dogs[i].get_text().strip()} vs {dogs[i+1].get_text().strip()}")
                     a = dogs[i].get_text().strip()
                     b = dogs[i+1].get_text().strip()
 
@@ -69,55 +66,74 @@ def proc_avb_w(driver, race_count, env_mens):
                     if dog_a != None and dog_b != None:
 
                         d_dogs_a, d_dogs_b, venc = f.compara_av(race_id, a, b )
-                        # print(venc[11])
-                        # print(venc[12])
-                        # print(d_dogs_a[12])
-                        # print(d_dogs_b[12])
-
-                        if venc[11] > venc[12] and abs(venc[11] - venc[12]) > 4 and venc[11] > 8.9 and venc[12] < 1.76:
+                        
+                        if venc[11] > venc[12] and abs(venc[11] - venc[12]) > 4 and venc[11] >= 9 and venc[12] < 1.76:
                             if (d_dogs_b[1] != n_dogs[0]) and (d_dogs_b[1] != n_dogs[1]):
                                 if ((d_dogs_a[12] == 2 or d_dogs_a[12] == 1) and (d_dogs_b[12] != 2) and (venc[11] and venc[12] != 0)) or ((d_dogs_a[12] == d_dogs_b[12] and d_dogs_a[12] != 0) and (venc[11] and venc[12] != 0)) :
-                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
-                                    ind = mensagem.find('@')
-                                    # print(ind)
-                                    v_mens = mensagem[:ind]
-                                    # print(v_mens)
-                                    if (v_mens in env_mens) is False:
-                                        bot.mens_telegram(mensagem)
-                                        env_mens.append(v_mens)
-                        elif venc[11] < venc[12] and abs(venc[11] - venc[12]) > 4 and venc[12] > 8.9 and venc[11] < 1.76:
+                                    if 1.59 > float(odds[i].get_text()) <= 2.00: 
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
+                                    elif venc[11] >= 10 and (float(odds[i].get_text()) < 2.26):
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
+
+                        elif venc[11] < venc[12] and abs(venc[11] - venc[12]) > 4 and venc[12] >= 9 and venc[11] < 1.76:
                             if (d_dogs_a[1] != n_dogs[0]) and (d_dogs_a[1] != n_dogs[1]):
                                 if (d_dogs_b[12] == 2 or d_dogs_b[12] == 1) and (d_dogs_a[12] != 2) and (venc[11] and venc[12] != 0) or ((d_dogs_a[12] == d_dogs_b[12] and d_dogs_b[12] != 0) and (venc[11] and venc[12] != 0)):
-                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
-                                    ind = mensagem.find('@')
-                                    # print(ind)
-                                    v_mens = mensagem[:ind]
-                                    # print(v_mens)
-                                    if (v_mens in env_mens) is False:
-                                        bot.mens_telegram(mensagem)
-                                        env_mens.append(v_mens)
-                        elif venc[11] > venc[12] and abs(venc[11] - venc[12]) > 1 and venc[11] > 4 and venc[12] < 1.76:
+                                    if 1.59 > float(odds[i+1].get_text()) <= 2.00:
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
+                                    elif venc[12] >= 10 and (float(odds[i].get_text()) < 2.26):
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
+                        elif venc[11] > venc[12] and abs(venc[11] - venc[12]) > 4 and venc[11] > 5 and venc[12] < 1:
                             if (d_dogs_b[1] != n_dogs[0]) and (d_dogs_b[1] != n_dogs[1]):
                                 if ((d_dogs_a[12] == 2 or d_dogs_a[12] == 1) and (d_dogs_b[12] != 2) and (venc[11] and venc[12] != 0)) :
-                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
-                                    ind = mensagem.find('@')
-                                    # print(ind)
-                                    v_mens = mensagem[:ind]
-                                    # print(v_mens)
-                                    if (v_mens in env_mens) is False:
-                                        bot.mens_telegram(mensagem)
-                                        env_mens.append(v_mens)
-                        elif venc[11] < venc[12] and abs(venc[11] - venc[12]) > 1 and venc[12] > 4 and venc[11] < 1.76:
+                                    if 1.59 > float(odds[i].get_text()) <= 1.90: 
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
+                        elif venc[11] < venc[12] and abs(venc[11] - venc[12]) > 4 and venc[12] > 5 and venc[11] < 1:
                             if (d_dogs_a[1] != n_dogs[0]) and (d_dogs_a[1] != n_dogs[1]):
                                 if (d_dogs_b[12] == 2 or d_dogs_b[12] == 1) and (d_dogs_a[12] != 2) and (venc[11] and venc[12] != 0) :
-                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
-                                    ind = mensagem.find('@')
-                                    # print(ind)
-                                    v_mens = mensagem[:ind]
-                                    # print(v_mens)
-                                    if (v_mens in env_mens) is False:
-                                        bot.mens_telegram(mensagem)
-                                        env_mens.append(v_mens)
+                                    if 1.59 > float(odds[i+1].get_text()) <= 1.90:
+                                        mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
+                                        ind = mensagem.find('@')
+                                        # print(ind)
+                                        v_mens = mensagem[:ind]
+                                        # print(v_mens)
+                                        if (v_mens in env_mens) is False:
+                                            bot.mens_telegram(mensagem)
+                                            env_mens.append(v_mens)
 
             driver, race_count = proc_prox_race(driver, race_count)
             time.sleep(3)
