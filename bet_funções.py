6349587dc231afbbb63d6e0a43676ef80d48754d
ryanr4057 <ryanr4057@gmail.com>
2023-03-07 10:03:32 -0300

    novo filtro

--- bet_funções.py
+++ bet_funções.py
@@ -10,6 +10,7 @@ import time
 import bot
 
 
+
 def proc_prox_race(driver, race_count):
     p_race = driver.find_elements(By.CLASS_NAME,"ipn-FixtureNoScores_Wrapper")
     if p_race is None:
@@ -35,7 +36,14 @@ def proc_prox_race(driver, race_count):
 def proc_avb_w(driver, race_count, env_mens):
 
     while True:
+        n_dogs = []
+
         avb_dogs = driver.find_elements(By.CLASS_NAME,"ir-RacingGreyhoundsMatchUpParticipant_Name")
+        all_dogs = driver.find_elements(By.CLASS_NAME,"ir-RacingGreyhoundsFixedOddsParticipant_ParticipantName ")
+        for i in range(0, len(all_dogs) -1):
+            n_dogs.append(all_dogs[i].text)
+
+        # print(n_dogs)
 
         if len(avb_dogs) > 0:
             html = driver.page_source
@@ -61,13 +69,10 @@ def proc_avb_w(driver, race_count, env_mens):
                     if dog_a != None and dog_b != None:
 
                         d_dogs_a, d_dogs_b, venc = f.compara_av(race_id, a, b )
-                        print(venc[11])
-                        print(venc[12])
-                        print(d_dogs_a[12])
-                        print(d_dogs_b[12])
-
-
-
+                        # print(venc[11])
+                        # print(venc[12])
+                        # print(d_dogs_a[12])
+                        # print(d_dogs_b[12])
 
                         if venc[11] > venc[12] and abs(venc[11] - venc[12]) > 4 and venc[11] > 8 and venc[12] < 1.76:
                             if ((d_dogs_a[12] == 2 or d_dogs_a[12] == 1) and (d_dogs_b[12] != 2) and (venc[11] and venc[12] != 0)) or ((d_dogs_a[12] == d_dogs_b[12] and d_dogs_a[12] != 0) and (venc[11] and venc[12] != 0)) :
@@ -89,6 +94,28 @@ def proc_avb_w(driver, race_count, env_mens):
                                 if (v_mens in env_mens) is False:
                                     bot.mens_telegram(mensagem)
                                     env_mens.append(v_mens)
+                        elif venc[11] > venc[12] and abs(venc[11] - venc[12]) > 1 and venc[11] > 4 and venc[12] < 1.76:
+                            if (d_dogs_b[1] != n_dogs[0]) and (d_dogs_b[1] != n_dogs[1]):
+                                if ((d_dogs_a[12] == 2 or d_dogs_a[12] == 1) and (d_dogs_b[12] != 2) and (venc[11] and venc[12] != 0)) :
+                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]})*  \nVENCE: \nTRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]}) \nOdd: @{odds[i].get_text()}💸 \nLink:{driver.current_url}"
+                                    ind = mensagem.find('@')
+                                    # print(ind)
+                                    v_mens = mensagem[:ind]
+                                    # print(v_mens)
+                                    if (v_mens in env_mens) is False:
+                                        bot.mens_telegram(mensagem)
+                                        env_mens.append(v_mens)
+                        elif venc[11] < venc[12] and abs(venc[11] - venc[12]) > 1 and venc[12] > 4 and venc[11] < 1.76:
+                            if (d_dogs_a[1] != n_dogs[0]) and (d_dogs_a[1] != n_dogs[1]):
+                                if (d_dogs_b[12] == 2 or d_dogs_b[12] == 1) and (d_dogs_a[12] != 2) and (venc[11] and venc[12] != 0) :
+                                    mensagem = f"RR TIPS - AvB: 🐶 \n{pista_nome[0]} {horario[0]} - ({nome[0]}) - {r_cat[0]} - {r_dist[0]}m \n*TRAP: {d_dogs_b[0]}- {d_dogs_b[1]} ({venc[12]})*\n  \nVENCE: \nTRAP: {d_dogs_a[0]}- {d_dogs_a[1]} ({venc[11]}) \nOdd: @{odds[i+1].get_text()}💸 \nLink:{driver.current_url}"
+                                    ind = mensagem.find('@')
+                                    # print(ind)
+                                    v_mens = mensagem[:ind]
+                                    # print(v_mens)
+                                    if (v_mens in env_mens) is False:
+                                        bot.mens_telegram(mensagem)
+                                        env_mens.append(v_mens)
 
             driver, race_count = proc_prox_race(driver, race_count)
             time.sleep(3)
