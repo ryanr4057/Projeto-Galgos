077b497bf84861f363c10e12051b633d15ab27e2
ryanr4057 <ryanr4057@gmail.com>
2023-04-30 17:05:39 -0300

    erro

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telegram
+import asyncio
+
+def mens_telegram(message_text):
+    api_token = '6051737713:AAExOZfgQOU3UgeYu9bP7w3Jv7pzED2PQ9k'
+
+    bot = telegram.Bot(api_token)
+
+    chat_id = '-1001725044307'
+    # message_text = 'Olá, este é um teste de mensagem enviada pelo bot do Telegram!'
+
+
+    async def enviar_mensagem():
+        await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='Markdown')
+
+    asyncio.run(enviar_mensagem())
+
+def mens_telegram_ia(message_text):
+    api_token = '6051737713:AAExOZfgQOU3UgeYu9bP7w3Jv7pzED2PQ9k'
+
+    bot = telegram.Bot(api_token)
+
+    chat_id = '-1001896788559'
+    # message_text = 'Olá, este é um teste de mensagem enviada pelo bot do Telegram!'
+
+
+    async def enviar_mensagem():
+        await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='Markdown')
+
+    asyncio.run(enviar_mensagem())
+
+
